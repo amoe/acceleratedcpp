5ce50c5cd44865f01540489c76ae8bacaa368ccd
David Banks <amoebae@gmail.com>
2017-12-06 10:12:51 +0000

    calculating row values with 'arithmetic'

--- loop_greeting.cc
+++ loop_greeting.cc
@@ -9,5 +9,15 @@ int main() {
     // This is the message, we can know its length at this point.
     const std::string greeting = "Hello, " + name + "!";
 
+    const int greeting_height = 1;
+    const int frame_height = 2;    // The top and the bottom use up one row each
 
+
+    // Padding applies to both the top and bottom of the greeting.
+    const int padding_top = 1;
+    const int padding_bottom = 1;
+    
+    const int padding_total = padding_top + padding_bottom;
+
+    
 }
