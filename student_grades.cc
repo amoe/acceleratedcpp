35ae879be47918e41cafb992f74c34aac2df0ed2
David Banks <amoebae@gmail.com>
2017-12-09 10:03:41 +0000

    adding loop

--- student_grades.cc
+++ student_grades.cc
@@ -14,4 +14,25 @@ int main() {
 
     cout << "Hello, " << name << "!" << endl;
 
+
+    cout << "Please enter your midterm and final exam grades (separated by spaces): ";
+    double midterm_grade;
+    double final_grade;
+
+    // I suppose that this syntax repeatedly reads because (cin >> x) yields cin.
+    cin >> midterm_grade >> final_grade;
+
+    // This uses the string pasting feature of the preprocessor.
+    cout << "Enter all your homework grades, "
+        "followed by end-of-file: ";
+
+    int count = 0;
+    double sum = 0;
+    double thisGrade;
+
+    // I suppose that cin returns 0 or NULL on EOF.
+    while (cin >> thisGrade) {
+        ++count;
+        sum += thisGrade;
+    }
 }
