5b724065503f5faa0bb77db3accdd2967c48ef94
David Banks <amoebae@gmail.com>
2019-10-27 16:37:08 +0000

    skeleton for writing xrefs

--- src/ch7_associative.cc
+++ src/ch7_associative.cc
@@ -19,7 +19,7 @@ using word_finder_t = vector<string> (*)(const string&);
 void demo_cross_reference_table(istream& input);
 void demo_word_count_using_map(istream& input);
 map<string, vector<int>> xref(istream& in, word_finder_t find_words);
-
+void print_xref_table(map<string, vector<int>> the_xref);
 
 const string multi_line_input = R"(
 Alice was beginning to get very tired of sitting by her sister on the
@@ -51,18 +51,56 @@ int main() {
 void demo_cross_reference_table(istream& input) {
     cout << "Generating xref" << endl;
     auto result = xref(input, split);
+    print_xref_table(result);
     cout << "Done" << endl;
+}
+
+void print_line_number_list(const vector<int>& line_numbers) {
+    typedef vector<int>::const_iterator iter_t;
+
+    // Loop and a half to emulate a join function.
+
+    iter_t it = line_numbers.begin();
+    cout << *it;
+    it++;   // scroll past it
+
+    while (it != line_numbers.end()) {
+        cout << ", " << *it;
+        it++;
+    }
+}
+
+void print_xref_table(map<string, vector<int>> the_xref) {
+    cout << "Table is listed:" << endl;
+
+    typedef map<string, vector<int>>::const_iterator iter_t;
+
+    for (iter_t it = the_xref.begin(); it != the_xref.end(); it++) {
+        string word = it->first;
+        vector<int> line_numbers = it->second;
+        
+        cout << word << ": [";
+        print_line_number_list(line_numbers);
+        cout << "]" << endl;
+    }
+    
+    cout << "Table listing done." << endl;
 }
 
 
 map<string, vector<int>> xref(istream& in, word_finder_t find_words) {
     map<string, vector<int>> result;
-
     string line;
 
     while (getline(in, line)) {
         vector<string> words = find_words(line);
         print_vector(words);
+
+
+        typedef vector<string>::const_iterator iter_t;
+        for (iter_t it = words.begin(); it != words.end(); it++) {
+            result[*it] = {0};
+        }
     }
 
 
