c0e760f8b34d1ccd0e836e6896162973bd11162c
David Banks <amoebae@gmail.com>
2020-04-16 06:13:19 +0100

    demo why swap required

--- src/ex8_4.cc
+++ src/ex8_4.cc
@@ -6,8 +6,14 @@ using std::swap;
 using std::cout;
 using std::endl;
 
+// March begin forward and end backwards.
+
+// When begin is at index 0, end is at index 4.
+// End gradually gets less, so the algorithm stops in the middle.
+// You must use swap because there are two assignments.
+
 template <class T>
-void my_reverse(T begin, T end) {
+void my_correct_reverse(T begin, T end) {
     while (begin != end) {
         --end;
         if (begin != end) {
@@ -17,15 +23,35 @@ void my_reverse(T begin, T end) {
     }
 }
 
+template <class T>
+void my_incorrect_reverse(T begin, T end) {
+    while (begin != end) {
+        --end;
+        if (begin != end) {
+            // This works...
+            *begin = *end;
+            // But this doesn't because *begin has already been overwritten.
+            *end = *begin;
+            begin++;
+        }
+    }
+}
+
 
 int main() {
     cout << "Starting." << endl;
 
-    vector<int> vec = {1,2,3,4,5};
+    cout << "Correct reverse:" << endl;
+    vector<int> vec1 = {1,2,3,4,5};
+    my_correct_reverse(vec1.begin(), vec1.end());
+    print_vector_int(vec1);
 
-    my_reverse(vec.begin(), vec.end());
-
-    print_vector_int(vec);
+    // The elements behind the midpoint are never modified when we
+    // use the incorrect version without swap.
+    cout << "Incorrect reverse:" << endl;
+    vector<int> vec2 = {1,2,3,4,5};
+    my_incorrect_reverse(vec2.begin(), vec2.end());
+    print_vector_int(vec2);
 
     cout << "End." << endl;
     return 0;
