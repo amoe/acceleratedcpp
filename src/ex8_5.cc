2ef29b1ca2538ffd2a3938ffc99d94ef7a7e7b1b
David Banks <amoebae@gmail.com>
2020-05-08 09:09:02 +0100

    restart this exercise

--- src/ex8_5.cc
+++ src/ex8_5.cc
@@ -1,12 +1,10 @@
 #include <iostream>
-#include <sstream>
-#include <string>
-#include <map>
-#include <vector>
-#include <cstdlib>
 #include "util.hh"
 #include "split.hh"
 #include "ex8_5.hh"
+
+using std::cout;
+using std::endl;
 
 const string demo_grammar = R"(
 <noun>          cat
@@ -25,40 +23,8 @@ const string demo_grammar = R"(
 <sentence>      the <noun-phrase> <verb> <location>
 )";
 
-Grammar read_grammar(istream& input) {
-    Grammar result;
-    string line;
-
-    while (getline(input, line)) {
-        vector<string> entry = split(line);
-        if (!entry.empty()) {
-            string key = entry.at(0);
-            // 'at' will not work here.
-            result[key].push_back(
-                // WTF is going on here?    Constructing a vector<string>
-                // from the list slice.  Equivalent to entry[1:] in python.
-                Rule(entry.begin() + 1, entry.end())
-            );
-        }
-    }
-
-    return result;
-}
-
-void demo_generating_sentences() {
-    cout << "I would generate some sentences." << endl;
-    stringstream sin(demo_grammar);
-    Grammar g = read_grammar(sin);
-    vector<string> sentence = generate_sentence(g);
-    print_vector(sentence);
-}
-
-
 int main() {
     cout << "Starting." << endl;
-
-    demo_generating_sentences();
-
     cout << "End." << endl;
     return 0;
 }
