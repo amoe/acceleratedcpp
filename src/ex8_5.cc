6406bb82a7ae9981f2801ed83453a5c6524952b3
David Banks <amoebae@gmail.com>
2020-05-08 18:26:18 +0100

    move implementation of xref into header file

--- src/ex8_5.cc
+++ src/ex8_5.cc
@@ -103,30 +103,6 @@ void demo_generating_sentences() {
     print_vector(sentence);
 }
 
-map<string, vector<int>> xref(
-    istream& in,
-    vector<string> find_words(const string&) = split
-) {
-    string line;
-    int line_number = 0;
-    map<string, vector<int>> ret;
-
-    while (getline(in, line)) {
-        ++line_number;
-
-        vector<string> words = find_words(line);
-        
-        for (
-            vector<string>::const_iterator it = words.begin();
-            it != words.end();
-            it++
-        ) {
-            ret[*it].push_back(line_number);
-        }
-    }
-
-    return ret;
-}
 
 void demo_cross_reference_table(istream& input) {
     cout << "Generating xref" << endl;
--- src/ex8_5.hh
+++ src/ex8_5.hh
@@ -4,6 +4,8 @@
 #include "grammar_types.hh"
 #include "random.hh"
 
+using std::istream;
+
 bool is_production(const string& s) {
     return s.size() > 1 && s[0] == '<' && s[s.size() - 1] == '>';
 }
@@ -33,5 +35,31 @@ template <typename T>
 void gen_sentence(const Grammar& g, T d) {
     gen_aux(g, "<sentence>", d);
 }
+
+map<string, vector<int>> xref(
+    istream& in,
+    vector<string> find_words(const string&) = split
+) {
+    string line;
+    int line_number = 0;
+    map<string, vector<int>> ret;
+
+    while (getline(in, line)) {
+        ++line_number;
+
+        vector<string> words = find_words(line);
+        
+        for (
+            vector<string>::const_iterator it = words.begin();
+            it != words.end();
+            it++
+        ) {
+            ret[*it].push_back(line_number);
+        }
+    }
+
+    return ret;
+}
+
 
 #endif /* EX8_5_HH */
