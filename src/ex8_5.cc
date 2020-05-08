ecb62673490dd795ce01b4a0b757260d997e4621
David Banks <amoebae@gmail.com>
2020-05-08 09:25:53 +0100

    read grammar

--- src/ex8_5.cc
+++ src/ex8_5.cc
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <sstream>
 #include <map>
+#include <string>
 #include "util.hh"
 #include "split.hh"
 #include "ex8_5.hh"
 
+using std::getline;
+using std::istream;
 using std::stringstream;
 using std::cout;
 using std::endl;
@@ -31,12 +34,28 @@ const string demo_grammar = R"(
 <sentence>      the <noun-phrase> <verb> <location>
 )";
 
+Grammar read_grammar(istream& in) {
+    Grammar result;
+    string line;
+
+    while (getline(in, line)) {
+        vector<string> entry = split(line);
+        if (!entry.empty()) {
+            string production_name = entry.at(0);
+            result[production_name].push_back({"foo"});
+        }
+    }
+
+    return result;
+}
+
 int main() {
     cout << "Starting." << endl;
 
     stringstream sin(demo_grammar);
-//    Grammar g = read_grammar(sin);
+    Grammar g = read_grammar(sin);
 
+    cout << "size read (number of unique productions) is " << g.size() << endl;
 
     cout << "End." << endl;
     return 0;
