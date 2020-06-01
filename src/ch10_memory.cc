8072be0709349dc05152eb6065739cf7eb99bb9b
David Banks <amoebae@gmail.com>
2020-06-01 19:19:55 +0100

    string literals

--- src/ch10_memory.cc
+++ src/ch10_memory.cc
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
 
 using std::vector;
 using std::back_inserter;
 using std::copy;
 using std::cout;
 using std::endl;
+using std::string;
 
 int square(int x) {
     return x * x;
+}
+
+size_t my_strlen(const char* p) {
+    size_t result = 0;
+    while (*p != '\0') {
+        result++;
+        p++;
+    }
+    return result;
 }
 
 int main() {
@@ -65,6 +76,21 @@ int main() {
         31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
     };
 
+
+    // String literals are just nul-terminated arrays.
+    const char hello[] = {'H', 'e', 'l', 'l', 'o', '\0'};
+
+    cout << "This is a C-style string: " << hello << endl;
+
+    cout << "Length is " << my_strlen(hello) << endl;
+
+    string s(hello);
+
+    cout << "This is a C++ string: " << s << endl;
+
+    // Construct a string from two iterators
+    string s2(hello, hello + my_strlen(hello));
+    cout << "This is another C++ string: " << s2 << endl;
 
     cout << "End." << endl;
     return 0;
