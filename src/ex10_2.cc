1e80302917c084194a029553fb1072a1f94b90f9
David Banks <amoebae@gmail.com>
2020-06-14 12:33:44 +0100

    refactor to header

--- src/ex10_2.cc
+++ src/ex10_2.cc
@@ -1,31 +1,7 @@
 #include <vector>
-#include <algorithm>
-#include <stdexcept>
-#include <iostream>
+#include "ex10_2.hh"
 
-using std::cout;
-using std::endl;
 using std::vector;
-using std::sort;
-
-double median(vector<double> vec) {
-    typedef vector<double>::size_type vec_sz;
-    
-    vec_sz size = vec.size();
-    if (size == 0)
-        throw std::domain_error("cannot take the median of an empty vector");
-
-     sort(vec.begin(), vec.end());
-
-     vec_sz mid = size / 2;
-
-    if  ((size % 2) == 0) {
-        return (vec[mid] + vec[mid-1]) / 2;
-    } else {
-        return vec[mid];
-    }
-}
-
 
 int main() {
     cout << "Starting." << endl;
