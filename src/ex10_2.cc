c40df9242a9cb4c3f49ae7aefa5695f5e77dd4bf
David Banks <amoebae@gmail.com>
2020-06-14 12:34:32 +0100

    Revert "refactor to header"
    This reverts commit 1e80302917c084194a029553fb1072a1f94b90f9.

--- src/ex10_2.cc
+++ src/ex10_2.cc
@@ -1,7 +1,31 @@
 #include <vector>
-#include "ex10_2.hh"
+#include <algorithm>
+#include <stdexcept>
+#include <iostream>
 
+using std::cout;
+using std::endl;
 using std::vector;
+using std::sort;
+
+double median(vector<double> vec) {
+    typedef vector<double>::size_type vec_sz;
+    
+    vec_sz size = vec.size();
+    if (size == 0)
+        throw std::domain_error("cannot take the median of an empty vector");
+
+     sort(vec.begin(), vec.end());
+
+     vec_sz mid = size / 2;
+
+    if  ((size % 2) == 0) {
+        return (vec[mid] + vec[mid-1]) / 2;
+    } else {
+        return vec[mid];
+    }
+}
+
 
 int main() {
     cout << "Starting." << endl;
