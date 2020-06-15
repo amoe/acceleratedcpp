6b816be883df03fd8028e4e95deced5d4dc493e0
David Banks <amoebae@gmail.com>
2020-06-15 19:16:32 +0100

    implement median for double array

--- src/ex10_2.cc
+++ src/ex10_2.cc
@@ -3,13 +3,26 @@
 #include <stdexcept>
 #include <iostream>
 
+using std::domain_error;
 using std::cout;
 using std::endl;
 using std::vector;
 using std::sort;
 
-double array_median(double* numbers) {
-    
+double array_median(double* numbers, size_t numbers_length) {
+    if (numbers_length == 0) {
+        throw domain_error("no sensible median");
+    }
+
+    sort(numbers, numbers + numbers_length);
+
+    size_t mid = numbers_length / 2;
+
+    if ((numbers_length % 2) == 0) {
+        return (numbers[mid] + numbers[mid - 1]) / 2;
+    } else {
+        return numbers[mid];
+    }
 }
 
 double vector_median(vector<double> vec) {
@@ -38,8 +51,9 @@ void test_vector_median() {
 }
 
 void test_array_median() {
-    double inputs[] = {-10, 1, 1.5, 2, 3, 10};
-    double found_median = array_median(inputs);
+    double inputs[] = {1.5, -10, 10, 2, 1,  3};
+    int inputs_length = sizeof(inputs) / sizeof(*inputs);
+    double found_median = array_median(inputs, inputs_length);
     cout << "Array median was " << found_median << endl;
 }
 
