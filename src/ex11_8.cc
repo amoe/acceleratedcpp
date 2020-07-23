df5507a2bda096ebb7b64c4da49f9fa2bd5a1583
David Banks <amoebae@gmail.com>
2020-07-23 07:58:02 +0100

    node class

--- src/ex11_8.cc
+++ src/ex11_8.cc
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <string>
 
 using std::cout;
 using std::endl;
+using std::string;
+
+template <typename T>
+class Node {
+public:
+    Node(T data): data(data) {}
+
+private:
+    T data;
+};
 
 int main() {
     cout << "Starting." << endl;
+
+    Node<string> n("foo");
 
     cout << "End." << endl;
     return 0;
