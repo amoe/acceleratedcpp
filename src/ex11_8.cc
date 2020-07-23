04ef8854ab7037dfa7de8d1bc83f354d2ac6f203
David Banks <amoebae@gmail.com>
2020-07-23 09:19:18 +0100

    11-8

--- src/ex11_8.cc
+++ src/ex11_8.cc
@@ -25,15 +25,57 @@ public:
         this->previous = previous;
     }
 
+    Node* get_next() {
+        return next;
+    }
+
+    Node* get_previous() {
+        return previous;
+    }
+
 private:
     const T& data;
     Node* next;
     Node* previous;
 };
 
+
+template <typename T>
+class ListIterator {
+public:
+    ListIterator(Node<T>* current_node): current_node(current_node) {
+    }
+
+    const T& operator*() {
+        return current_node->get_data();
+    }
+
+    void operator++(int n) {
+        current_node = current_node->get_next();
+    }
+
+    bool operator==(const ListIterator& rhs) {
+        return current_node == rhs.current_node;
+    }
+
+    bool operator!=(const ListIterator& rhs) {
+        return !(*this == rhs);
+    }
+
+    Node<T>* get_current_node() {
+        return current_node;
+    }
+
+
+private:    
+    Node<T>* current_node;
+};
+
 template <typename T>
 class List {
 public:
+    using iterator = ListIterator<T>;
+
     List(): front_node(0), back_node(0) {
         
     }
@@ -43,10 +85,10 @@ public:
         Node<T>* node_pointer = new Node<T>(data);
 
         if (front_node == 0) {
-            cout << "empty" << endl;
             front_node = node_pointer;
             back_node = node_pointer;
         } else {
+            node_pointer->set_previous(back_node);
             back_node->set_next(node_pointer);
             back_node = node_pointer;
         }
@@ -55,11 +97,31 @@ public:
     const T& get_front() {
         return front_node->get_data();
     }
-    
+
+    ListIterator<T> begin() {
+        return ListIterator<T>(front_node);
+    }
+
+    ListIterator<T> end() {
+        // Slightly subtle, have a think about why this works.
+        return ListIterator<T>(0);
+    }
+
+    void erase(iterator pos) {
+        Node<T>* node = pos.get_current_node();
+        Node<T>* previous = node->get_previous();
+        Node<T>* next = node->get_next();
+        previous->set_next(next);
+        delete node;
+    }
+
+
 private:
     Node<T>* front_node;
     Node<T>* back_node;
 };
+
+
 
 
 void manual_link() {
@@ -81,13 +143,35 @@ void manual_link() {
 int main() {
     cout << "Starting." << endl;
 
-    manual_link();
+    // manual_link();
 
-    List<string> foo;
-    foo.push_back("fry");
+    List<string> my_list;
+    my_list.push_back("fry");
+    my_list.push_back("bender");
+    my_list.push_back("leela");
 
-    string out = foo.get_front();
+    string out = my_list.get_front();
     cout << "out is " << out << endl;
+
+
+    List<string>::iterator it = my_list.begin();
+
+    while (it != my_list.end()) {
+        cout << *it << endl;
+        it++;
+    }
+
+    List<string>::iterator it2 = my_list.begin();
+    it2++;
+    my_list.erase(it2);
+    
+
+
+    List<string>::iterator it3 = my_list.begin();
+    while (it3 != my_list.end()) {
+        cout << *it3 << endl;
+        it3++;
+    }
 
     cout << "End." << endl;
     return 0;
