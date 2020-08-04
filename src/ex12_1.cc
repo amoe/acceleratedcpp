37e440828ef212ba8e2905e039aac04ed2931ae4
David Banks <amoebae@gmail.com>
2020-08-04 23:28:13 +0100

    all exercises

---
+++ src/ex12_1.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "ex12_1.hh"
+
+using std::cout;
+using std::endl;
+
+int main() {
+    cout << "Starting." << endl;
+
+    cout << "End." << endl;
+    return 0;
+}
---
+++ src/ex12_1.hh
@@ -0,0 +1,15 @@
+#ifndef EX12_1_HH
+#define EX12_1_HH
+
+template <typename T> 
+class StrV {
+public:
+    explicit StrV(const char* initializer) {
+    }
+
+private:
+    T* data;
+};
+
+
+#endif /* EX12_1_HH */
