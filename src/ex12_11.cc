adb1bd289ceaf504fe2a855ea07080a52c68eecb
David Banks <amoebae@gmail.com>
2020-09-01 08:47:37 +0100

    finishing ex12-11

--- src/ex12_11.cc
+++ src/ex12_11.cc
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include "ex12_11.hh"
 
+using std::max;
 using std::list;
 using std::find_if;
 using std::cout;
@@ -87,6 +88,110 @@ void test_ch6_split() {
     }
 }
 
+Str::size_type width(const vector<Str>& v) {
+    Str::size_type maxlen = 0;
+
+    for (vector<Str>::size_type i = 0; i < v.size(); i++) {
+        maxlen = max(maxlen, v[i].size());
+    }
+
+    return maxlen;
+}
+
+
+vector<Str> frame(const vector<Str>& words) {
+    vector<Str> result;
+    Str::size_type maxlen = width(words);
+    Str border(maxlen + 4, '*');
+
+    result.push_back(border);
+
+    for (vector<Str>::size_type i = 0; i < words.size(); i++) {
+        Str padding(maxlen - words[i].size(), ' ');
+        
+        result.push_back("* "  + words[i] + padding + " *");
+    }
+
+    result.push_back(border);
+
+    return result;
+}
+
+vector<Str> vcat(const vector<Str>& top, const vector<Str>& bottom) {
+    vector<Str> result = top;
+
+    for (
+        vector<Str>::const_iterator iter = bottom.begin();
+        iter != bottom.end();
+        iter++
+    ) {
+        result.push_back(*iter);
+    }
+
+    return result;
+}
+
+vector<Str> hcat(const vector<Str>& left, const vector<Str>& right) {
+    vector<Str> result;
+
+    Str::size_type widthLeft = width(left) + 1;
+    // i will track the rows of left, j the rows of right
+    vector<Str>::size_type i = 0, j = 0;
+
+    while (i != left.size() || j != right.size()) {
+        Str s;
+
+        // i may have already run out at any stage during the loop.
+        if (i != left.size()) {
+            s = left[i];
+            i++;
+        }
+
+        s += Str(widthLeft - s.size(), ' ');
+
+        if (j != right.size()) {
+            s += right[j];
+            j++;
+        }
+
+        
+        result.push_back(s);
+    }
+
+    return result;
+}
+
+void test_frame() {
+    vector<Str> result;
+
+    result.push_back("The");
+    result.push_back("quick");
+    result.push_back("brown");
+    result.push_back("fox");
+    result.push_back("jumped");
+    result.push_back("over");
+    result.push_back("the");
+    result.push_back("lazy");
+    result.push_back("dog");
+
+    using iter_t = vector<Str>::const_iterator;
+
+    vector<Str> r1 = frame(result);
+    for (iter_t it = r1.begin(); it != r1.end(); it++) {
+        std::cout << *it << std::endl;
+    }
+
+    vector<Str> r2 = vcat(result, r1);
+    for (iter_t it = r2.begin(); it != r2.end(); it++) {
+        std::cout << *it << std::endl;
+    }
+
+    vector<Str> r3 = hcat(result, r1);
+    for (iter_t it = r3.begin(); it != r3.end(); it++) {
+        std::cout << *it << std::endl;
+    }
+}
+
 
 int main() {
     cout << "Starting." << endl;
@@ -94,6 +199,8 @@ int main() {
     test_substr();
     test_ch5_split();
     test_ch6_split();
+
+    test_frame();
 
     cout << "End." << endl;
     return 0;
