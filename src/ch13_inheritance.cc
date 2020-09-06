b86af8e5fb27e5e325271756b31d5ee1be8c7c47
David Banks <amoebae@gmail.com>
2020-09-06 09:43:20 +0100

    move decl into header and omit using declarations

--- src/ch13_inheritance.cc
+++ src/ch13_inheritance.cc
@@ -9,33 +9,6 @@ using std::istream;
 using std::cout;
 using std::endl;
 
-class CoreStudent {
-public:
-    CoreStudent();
-    CoreStudent(istream&);
-    string name() const;
-    istream& read(istream&);
-    double grade() const;
-
-protected:
-    istream& read_common(istream&);
-    double midterm_grade, final_grade;
-    vector<double> homework;
-
-private:
-    string n;
-};
-
-class GradStudent: public CoreStudent {
-public:
-    GradStudent();
-    GradStudent(istream&);
-    double grade() const;    // Note that we re-declare methods we will override
-    istream& read(istream&);
-
-private:
-    double thesis;
-};
 
 int main() {
     cout << "Starting." << endl;
--- src/ch13_inheritance.hh
+++ src/ch13_inheritance.hh
@@ -1,6 +1,33 @@
 #ifndef CH13_INHERITANCE_HH
 #define CH13_INHERITANCE_HH
 
+class CoreStudent {
+public:
+    CoreStudent();
+    CoreStudent(std::istream&);
+    std::string name() const;
+    std::istream& read(std::istream&);
+    double grade() const;
+
+protected:
+    std::istream& read_common(std::istream&);
+    double midterm_grade, final_grade;
+    std::vector<double> homework;
+
+private:
+    std::string n;
+};
+
+class GradStudent: public CoreStudent {
+public:
+    GradStudent();
+    GradStudent(std::istream&);
+    double grade() const;    // Note that we re-declare methods we will override
+    std::istream& read(std::istream&);
+
+private:
+    double thesis;
+};
 
 
 #endif /* CH13_INHERITANCE_HH */
