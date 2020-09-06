99b1e318a9ef98ceacd807d81150a683d607caf5
David Banks <amoebae@gmail.com>
2020-09-06 09:30:22 +0100

    declaration for corestudent

--- src/ch13_inheritance.cc
+++ src/ch13_inheritance.cc
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include "ch13_inheritance.hh"
 
+using std::string;
+using std::vector;
+using std::istream;
 using std::cout;
 using std::endl;
+
+class CoreStudent {
+public:
+    CoreStudent();
+    CoreStudent(istream&);
+    string name() const;
+    istream& read(istream&);
+    double grade() const;
+
+private:
+    istream& read_common(istream&);
+    string n;
+    double midterm_grade, final_grade;
+    vector<double> homework;
+};
 
 int main() {
     cout << "Starting." << endl;
