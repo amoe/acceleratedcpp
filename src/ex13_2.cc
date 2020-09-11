3f7e93f2c24e5c39ac189c5e90f8360301d79fe2
David Banks <amoebae@gmail.com>
2020-09-11 08:37:25 +0100

    constructor checks

--- src/ex13_2.cc
+++ src/ex13_2.cc
@@ -1,17 +1,20 @@
 #include <iostream>
+#include <numeric>
 #include <vector>
 #include "ex13_2.hh"
 #include "read_hw.hh"
 #include "median.hh"
 #include "grading_functions.hh"
 
+using std::min;
 using std::istream;
 using std::vector;
 using std::cout;
 using std::endl;
+using std::cerr;
 
 CoreStudent::CoreStudent(): midterm_grade(0), final_grade(0) {
-    cout << "  CoreStudent::CoreStudent()" << endl;
+    cerr << "  CoreStudent::CoreStudent()" << endl;
 }
 
 double CoreStudent::grade() const {
@@ -29,16 +32,42 @@ istream& CoreStudent::read(istream& in) {
     return in;
 }
 
+GradStudent::GradStudent(): thesis(0) {
+    cerr << "  GradStudent::GradStudent()" << endl;
+}
+
+GradStudent::GradStudent(istream& is) {
+    cerr << "  GradStudent::GradStudent(istream&)" << endl;
+    read(is);
+}
+
+istream& GradStudent::read(istream& in) {
+    CoreStudent::read_common(in);
+    in >> thesis;
+    read_hw(in, CoreStudent::homework);
+    return in;
+}
+
+double GradStudent::grade() const {
+    return min(CoreStudent::grade(), thesis);
+}
+
+
 int main() {
     cout << "Starting." << endl;
 
     
-    // Should call CoreStudent::CoreStudent()
+    // Should call CoreStudent::CoreStudent() ✓
     CoreStudent* p1 = new CoreStudent;
 
-    // CoreStudent* p2 = new GradStudent;
-    // CoreStudent s1;
-    // GradStudent s2;
+    // Should call CoreStudent::CoreStudent, then GradStudent::GradStudent() ✓
+    CoreStudent* p2 = new GradStudent;
+    
+    // Should call CoreStudent::CoreStudent() ✓
+    CoreStudent s1;
+    
+    // Should call CoreStudent::CoreStudent, then GradStudent::GradStudent() ✓
+    GradStudent s2;
     
     // p1->grade();
     // p1->name();
