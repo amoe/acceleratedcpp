50de60167f45019020f8ca3a68f99f97aa5ca76b
David Banks <amoebae@gmail.com>
2020-09-24 22:34:06 +0100

    rewriting polymorphic grading program

--- src/ch14_automatic.cc
+++ src/ch14_automatic.cc
@@ -1,10 +1,81 @@
 #include <iostream>
 #include <stdexcept>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include "median.hh"
+#include "read_hw.hh"
+#include "grading_functions.hh"
 #include "ch14_automatic.hh"
 
+using std::sort;
+using std::max;
+using std::min;
+using std::istream;
+using std::stringstream;
+using std::string;
+using std::vector;
 using std::cout;
 using std::endl;
 using std::runtime_error;
+
+const string students_input = R"(
+U Gamlin 94 89 14 96 16 63
+U Capener 7 10 32 68 61 76
+G Droney 31 75 83 81 54 18 87 
+G Zutell 99 99 26 99 99 99 99 
+)";
+
+bool compare_Core_handles(
+    const Handle<CoreStudent>& s1, const Handle<CoreStudent>& s2
+) {
+    return s1->name() < s2->name();
+}
+
+CoreStudent::CoreStudent(): midterm_grade(0), final_grade(0) {
+}
+
+CoreStudent::CoreStudent(istream& is) {
+    read(is);
+}
+
+string CoreStudent::name() const {
+    return n;
+}
+
+double CoreStudent::grade() const {
+    return ::grade(midterm_grade, final_grade, homework);
+}
+
+istream& CoreStudent::read_common(istream& in) {
+    in >> n >> midterm_grade >> final_grade;
+    return in;
+}
+
+istream& CoreStudent::read(istream& in) {
+    read_common(in);
+    read_hw(in, homework);
+    return in;
+}
+
+GradStudent::GradStudent(): thesis(0) {
+}
+
+GradStudent::GradStudent(istream& is) {
+    read(is);
+}
+
+istream& GradStudent::read(istream& in) {
+    CoreStudent::read_common(in);
+    in >> thesis;
+    read_hw(in, CoreStudent::homework);
+    return in;
+}
+
+double GradStudent::grade() const {
+    return min(CoreStudent::grade(), thesis);
+}
 
 template <typename T>
 Handle<T>& Handle<T>::operator=(const Handle& rhs) {
@@ -38,9 +109,7 @@ T* Handle<T>::operator->() const {
     }
 }
 
-int main() {
-    cout << "Starting." << endl;
-
+void dog_test() {
     Dog* dog = new Dog;
     
     Handle<Dog> handle1(dog);
@@ -48,7 +117,6 @@ int main() {
 
     cout << "Dog's val is " << (*handle1).val << endl;
 
-    
     // Assignment should fully copy the Dog, using the synthetic copy
     // constructor created by the compiler.
     Handle<Dog> handle2;
@@ -57,7 +125,43 @@ int main() {
 
     cout << "Handle1's val is " << (*handle1).val << endl;
     cout << "Handle2's val is " << (*handle2).val << endl;
+}
 
+void grading_test() {
+    vector<Handle<CoreStudent>> students;
+    Handle<CoreStudent> record;
+    char ch;
+    string::size_type maxlen = 0;
+    stringstream sin(students_input);
+
+    
+    while (sin >> ch) {
+        if (ch == 'U') {
+            record = new CoreStudent;
+        } else {
+            record = new GradStudent;
+        }
+
+        record->read(sin);
+
+        maxlen = max(maxlen, record->name().size());
+        students.push_back(record);
+    }
+
+    cout << "Read " << students.size() << " students." << endl;
+
+    sort(students.begin(), students.end(), compare_Core_handles);
+
+    
+}
+
+
+
+int main() {
+    cout << "Starting." << endl;
+
+    dog_test();
+    grading_test();
     
     cout << "End." << endl;
     return 0;
--- src/ch14_automatic.hh
+++ src/ch14_automatic.hh
@@ -43,5 +43,49 @@ private:
     T* ptr;
 };
 
+class CoreStudent {
+public:
+    CoreStudent();
+    CoreStudent(std::istream&);
+    std::string name() const;
+    virtual std::istream& read(std::istream&);
+    virtual double grade() const;
+    virtual ~CoreStudent() { }
+    virtual CoreStudent* clone() const {
+        return new CoreStudent(*this);
+    }
+    void regrade(double d) {
+        final_grade = d;
+    }
+    
+protected:
+    std::istream& read_common(std::istream&);
+    double midterm_grade, final_grade;
+    std::vector<double> homework;
+
+
+private:
+    std::string n;
+};
+
+class GradStudent: public CoreStudent {
+public:
+    GradStudent();
+    GradStudent(std::istream&);
+    double grade() const;    // Note that we re-declare methods we will override
+    std::istream& read(std::istream&);
+    GradStudent* clone() const {
+        return new GradStudent(*this);
+    }
+    void regrade(double d1, double d2) {
+        final_grade = d1;
+        thesis = d2;
+    }
+
+private:
+    double thesis;
+};
+
+
 
 #endif /* CH14_AUTOMATIC_HH */
