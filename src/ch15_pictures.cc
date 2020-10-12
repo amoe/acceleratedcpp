44a7c5974cae58117b236a213fc4cab05847158c
David Banks <amoebae@gmail.com>
2020-10-12 08:28:47 +0100

    initial code

--- src/ch15_pictures.cc
+++ src/ch15_pictures.cc
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include "handles.hh"
 
+using std::string;
+using std::vector;
 using std::cout;
 using std::endl;
+
+class BasePicture {
+};
+
+class StringPicture: public BasePicture {
+};
+
+class FramePicture: public BasePicture {
+};
+
+class VerticallyConcatenatedPicture: public BasePicture {
+};
+
+class HorizontallyConcatenatedPicture: public BasePicture {
+};
+
+class Picture {
+public:
+    Picture(const vector<string>& = vector<string>());
+
+private:
+    ControllableHandle<BasePicture> picture;
+};
+
+
 
 int main() {
     cout << "Starting." << endl;
