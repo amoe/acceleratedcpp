a30947637c1d216ab69dae3ba172c2462f31da7c
David Banks <amoebae@gmail.com>
2020-11-02 21:06:37 +0000

    quine attempt

--- src/ex16_1.cc
+++ src/ex16_1.cc
@@ -1,11 +1,48 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
-using std::cout;
+using std::string;
+using std::vector;
 using std::endl;
+using std::cout;
 
 int main() {
-    cout << "Starting." << endl;
+    vector<string> a;
+    // marker
+    a.push_back("#include <iostream>");
+    a.push_back("#include <vector>");
+    a.push_back("#include <string>");
+    a.push_back("");
+    a.push_back("using std::string;");
+    a.push_back("using std::vector;");
+    a.push_back("using std::endl;");
+    a.push_back("using std::cout;");
+    a.push_back("");
+    a.push_back("int main() {");
+    a.push_back("    vector<string> a;");
+    a.push_back("    // marker");
+    a.push_back("    for (vector<string>::const_iterator it1 = a.begin(); it1 != a.end(); it1++) {");
+    a.push_back("        cout << *it1 << endl;");
+    a.push_back("");
+    a.push_back("        if (*it1 == \"    // marker\") {");
+    a.push_back("            for (vector<string>::const_iterator it2 = a.begin(); it2 != a.end(); it2++) {");
+    a.push_back("                cout << \\\"    a.push_back(\"\" << *it2 << \"\");\\\" << endl;");
+    a.push_back("            }");
+    a.push_back("        }");
+    a.push_back("    }");
+    a.push_back("    for (vector<string>::const_iterator it1 = a.begin(); it1 != a.end(); it1++) {");
+    a.push_back("        cout << *it1 << endl;");
+    a.push_back("    }");
+    a.push_back("}");
 
-    cout << "End." << endl;
-    return 0;
+    for (vector<string>::const_iterator it1 = a.begin(); it1 != a.end(); it1++) {
+        cout << *it1 << endl;
+
+        if (*it1 == "    // marker") {
+            for (vector<string>::const_iterator it2 = a.begin(); it2 != a.end(); it2++) {
+                cout << "    a.push_back(\"" << *it2 << "\");" << endl;
+            }
+        }
+    }
 }
